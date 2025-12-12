c59a8df41839997ec1483f306acc3527a50a888c
simonvarey <simonvarey@gmail.com>
2025-12-12 14:08:33 -0500

    Converted chartDataIsOrdered to chartDataIsUnivalent, made generic over indep key

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,22 +1,26 @@
-export function collectXs(data: DatapointManifest[]): Set<string> {
+import { AllSeriesData, DatapointManifest } from "@fizz/paramanifest";
+
+export function collectIndeps(data: DatapointManifest[], indepKey: string): Set<string> {
   return new Set(...data.map((datapoint) => datapoint.x));
 }
 
+type Primitive = undefined | null | boolean | number | string | symbol | BigInt;
+
 // TODO: specify that T must be primitive
-function setEquals<T>(lhs: Set<T>, rhs: Set<T>) {
+function setEquals<T extends Primitive>(lhs: Set<T>, rhs: Set<T>) {
   return lhs.size === rhs.size && lhs.isSubsetOf(rhs);
 }
 
-export function chartDataIsOrdered(data: AllSeriesData): boolean {
+export function chartDataIsUnivalent(data: AllSeriesData, indepKey: string): boolean {
   let chartXs: Set<string> | null = null;
   for (const key in data) {
     const seriesData = data[key];
-    const xs = collectXs(seriesData);
-    // Series not ordered
+    const xs = collectIndeps(seriesData, indepKey);
+    // Series not univalent
     if (xs.size !== seriesData.length) {
       return false;
     }
-    // Chart not ordered
+    // Chart not univalent
     if (chartXs === null) {
       chartXs = xs;
     } else if (!setEquals(xs, chartXs)) {
